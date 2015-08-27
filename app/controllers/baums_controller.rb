54706ccf7a21ed8074687f2ac6d541b29179ff40
sashasoft <kovalenko.alexandrs@gmail.com>
2015-08-28 01:11:33 +0300

    Доделал допформу для СМХ

--- app/controllers/baums_controller.rb
+++ app/controllers/baums_controller.rb
@@ -61,9 +61,18 @@ class BaumsController < ApplicationController
     end
 
     if params[:plfund].to_f > 0
-      @sum = @sum + Baum.find_by(label: :plfund).price * params[:plfund].to_f;
       temp = Baum.find_by(label: :plfund)
-      @hh.push({"name" => temp.name, "value" => temp.price * params[:plfund].to_f})
+    
+      if params[:fundament1400]
+         @sum = @sum + temp.price * params[:plfund].to_f * Baum.find_by(label: :fundament1400).price.to_f;
+         @hh.push({"name" => Baum.find_by(label: :fundament1400).name.to_s, "value" => temp.price * params[:plfund].to_f*Baum.find_by(label: :fundament1400).price.to_f})
+      elsif params[:fundament1200]
+        @sum = @sum + temp.price * params[:plfund].to_f * Baum.find_by(label: :fundament1200).price.to_f;
+         @hh.push({"name" => Baum.find_by(label: :fundament1200).name.to_s, "value" => temp.price * params[:plfund].to_f*Baum.find_by(label: :fundament1200).price.to_f})
+      else
+        @sum = @sum + temp.price * params[:plfund].to_f;
+        @hh.push({"name" => temp.name, "value" => temp.price * params[:plfund].to_f})
+      end
     end
     
     
@@ -212,8 +221,125 @@ class BaumsController < ApplicationController
     end
     
     if params[:vysotadoma].to_f > 0 && params[:perimetrkrovli].to_f > 0
+      temp = Baum.find_by(label: :vysotadoma)
+      temp1 = Baum.find_by(label: :perimetrkrovli)
+      @sum = @sum + temp.price*params[:vysotadoma].to_f
+      @hh.push({"name" => temp.name, "value" => temp.price*params[:vysotadoma].to_f})
+      @sum = @sum + temp1.price*params[:perimetrkrovli].to_f
+      @hh.push({"name" => temp1.name, "value" => temp1.price*params[:perimetrkrovli].to_f})
     end
     
+    if params[:okna].to_f > 0
+      temp = Baum.find_by(label: :okna)
+      @sum = @sum + temp.price * params[:okna].to_f
+      @hh.push({"name" => temp.name, "value" => temp.price*params[:okna].to_f})
+    end
+    if params[:moskitka].to_f > 0
+      temp = Baum.find_by(label: :moskitka)
+      @sum = @sum + temp.price * params[:moskitka].to_f
+      @hh.push({"name" => temp.name, "value" => temp.price*params[:moskitka].to_f})
+    end
+    if params[:dveri].to_f > 0
+      temp = Baum.find_by(label: :dveri)
+      @sum = @sum + temp.price * params[:dveri].to_f
+      @hh.push({"name" => temp.name, "value" => temp.price*params[:dveri].to_f})
+    end
+    
+    if params[:otmostka800].to_f > 0
+      temp = Baum.find_by(label: :otmostka800)
+      @sum = @sum + temp.price.to_f
+      @hh.push({"name" => temp.name, "value" => temp.price*params[:otmostka800].to_f})
+    end
+    if params[:otmostka1000].to_f > 0
+      temp = Baum.find_by(label: :otmostka1000)
+      @sum = @sum + temp.price.to_f
+      @hh.push({"name" => temp.name, "value" => temp.price*params[:otmostka1000].to_f})
+    end
+    
+    if params[:podokonniki].to_f > 0
+      temp = Baum.find_by(label: :podokonniki)
+      @sum = @sum + temp.price.to_f
+      @hh.push({"name" => temp.name, "value" => temp.price.to_f*params[:podokonniki].to_f})
+    end
+    
+    if params[:moskitka2].to_f > 0
+      temp = Baum.find_by(label: :moskitka2)
+      @sum = @sum + temp.price * params[:moskitka2].to_f
+      @hh.push({"name" => temp.name, "value" => temp.price*params[:moskitka2].to_f})
+    end
+    
+    
+    if params[:otliv].to_f > 0
+      temp = Baum.find_by(label: :otliv)
+      @sum = @sum + temp.price * params[:otliv].to_f
+      @hh.push({"name" => temp.name, "value" => temp.price*params[:otliv].to_f})
+    end
+    
+    if params[:stupenki].to_f > 0
+      temp = Baum.find_by(label: :stupenki)
+      @sum = @sum + temp.price.to_f
+      @hh.push({"name" => temp.name, "value" => temp.price})
+    end
+    
+    if params[:naves].to_f > 0
+      temp = Baum.find_by(label: :naves)
+      @sum = @sum + temp.price.to_f
+      @hh.push({"name" => temp.name, "value" => temp.price})
+    end
+    
+    
+    if params[:tsokol].to_f > 0
+      if params[:tsokolklinker]
+        temp = Baum.find_by(label: :tsokolklinker)
+        @sum = @sum + temp.price * params[:tsokol].to_f
+        @hh.push({"name" => temp.name, "value" => temp.price*params[:tsokol].to_f})
+      end
+      if params[:tsokolpeschanik]
+        temp = Baum.find_by(label: :tsokolpeschanik)
+        @sum = @sum + temp.price * params[:tsokol].to_f
+        @hh.push({"name" => temp.name, "value" => temp.price*params[:tsokol].to_f})
+      end
+      if params[:tsokolplitka]
+        temp = Baum.find_by(label: :tsokolplitka)
+        @sum = @sum + temp.price * params[:tsokol].to_f
+        @hh.push({"name" => temp.name, "value" => temp.price*params[:tsokol].to_f})
+      end
+    end
+    
+    if params[:mezhkom].to_f > 0
+        temp = Baum.find_by(label: :mezhkom)
+        @sum = @sum + temp.price * params[:mezhkom].to_f
+        @hh.push({"name" => temp.name, "value" => temp.price*params[:mezhkom].to_f})
+    end
+    
+    if params[:lakpol].to_f > 0
+        temp = Baum.find_by(label: :lakpol)
+        @sum = @sum + temp.price * params[:lakpol].to_f
+        @hh.push({"name" => temp.name, "value" => temp.price*params[:lakpol].to_f})
+    end
+    if params[:lakpotolok].to_f > 0
+        temp = Baum.find_by(label: :lakpotolok)
+        @sum = @sum + temp.price * params[:lakpotolok].to_f
+        @hh.push({"name" => temp.name, "value" => temp.price*params[:lakpotolok].to_f})
+    end
+    
+    if params[:lestnica].to_f > 0
+      if params[:v1]
+        temp = Baum.find_by(label: :v1)
+        @sum = @sum + temp.price * params[:lestnica].to_f
+        @hh.push({"name" => temp.name, "value" => temp.price*params[:lestnica].to_f})
+      end
+      if params[:v2]
+        temp = Baum.find_by(label: :v2)
+        @sum = @sum + temp.price * params[:lestnica].to_f
+        @hh.push({"name" => temp.name, "value" => temp.price*params[:lestnica].to_f})
+      end
+      if params[:v3]
+        temp = Baum.find_by(label: :v3)
+        @sum = @sum + temp.price * params[:lestnica].to_f
+        @hh.push({"name" => temp.name, "value" => temp.price*params[:lestnica].to_f})
+      end
+    end
     ##################
     ##################
     
